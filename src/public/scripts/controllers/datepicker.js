646edbe1a61dce38920e5876047c8672bf7775eb
Max White <maxjwhite22@gmail.com>
2016-01-09 14:56:15 -0500

    form styles

--- src/public/scripts/controllers/datepicker.js
+++ src/public/scripts/controllers/datepicker.js
@@ -1,3 +1,5 @@
+'use strict';
+
 angular.module('trackerApp').controller('DateTimePickerCtrl', function ($scope) {
 
   //date stuff
---
+++ src/public/scripts/controllers/form.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('trackerApp').controller('FormCtrl', function ($scope) {
+
+  $scope.titlePlaceholder = "+Title";
+  $scope.inputSize = $scope.titlePlaceholder.length;
+  $scope.change = function(text){
+    // var p = angular.element(document.createElement('p'));
+    // p.text(text);
+    // console.log(p.text());
+
+    if (text.length === 0){
+      $scope.inputSize = $scope.titlePlaceholder.length;
+    } else {
+      $scope.inputSize = text.length + 1;
+    }
+  };
+});
