c2a913074a81a32eafb932058c8814a3f8320d31
Max White <maxjwhite22@gmail.com>
2016-01-11 20:03:25 -0500

    adding report to array on save

--- src/public/scripts/controllers/form.js
+++ src/public/scripts/controllers/form.js
@@ -4,6 +4,9 @@ angular.module('trackerApp')
 .controller('FormCtrl', function ($scope) {
 
   $scope.newReport = {};
+  $scope.currentUser = {
+    name:"Username"
+  };
 
   //title
   $scope.titlePlaceholder = "+Title";
@@ -22,6 +25,25 @@ angular.module('trackerApp')
 
 
   $scope.save = function(){
-    console.log($scope.newReport);
+    var date = $scope.newReport.date;
+    var time = $scope.newReport.time;
+    var report = {
+      title:$scope.newReport.title,
+      time: new Date(date.getFullYear(), date.getMonth(), date.getDate(),
+               time.getHours(), time.getMinutes(), time.getSeconds()),
+      author:$scope.currentUser.name,
+      students:$scope.newReport.students,
+      description:$scope.newReport.notes,
+      tags:$scope.newReport.tags
+    };
+    $scope.$parent.reports.push(report);
+    $scope.newReport = {
+      title:"",
+      date: new Date(),
+      time: new Date(),
+      students: [],
+      tags: [],
+      notes: ""
+    };
   };
 });
